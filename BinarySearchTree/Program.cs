850ba65f91aa6e7a444c53d7cc0d80c2beae8b73
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-12-26 18:01:23 +0700

    add switch case

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -130,7 +130,38 @@ namespace BinarySearchTree
                 Console.WriteLine("\nEnter your choice (1-5) : ");
                 char ch = Convert.ToChar(Console.ReadLine());
                 Console.WriteLine();
-                
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("enter a word:");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("invalid option");
+                            break;
+                        }
+                }
             }
         }
     }       
