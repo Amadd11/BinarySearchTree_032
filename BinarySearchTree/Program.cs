08c2b366f5232fc6e5421b9fbc163fc6d66f0185
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-12-26 17:40:45 +0700

    add class binary tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,20 +6,34 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
+    /* A node class consi 
+
+
+       */
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
+
+    class BinaryTree
+    {
+       
+    }
+  
     class Program
     {
-        /* A node class consi 
-         
-         
-         */
-        class node
-        {
-            public string info;
-            public node lchild;
-            public node rchild;
-           
-            //constructor for the node class
-        }
+      
         static void Main(string[] args)
         {
         }
