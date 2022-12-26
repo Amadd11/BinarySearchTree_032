e901398b7807534d5369a3fc7c3b8bb8bdd87c99
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-12-26 17:14:11 +0700

    constructor for the node class

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,7 +8,18 @@ namespace BinarySearchTree
 {
     class Program
     {
-        /* A
+        /* A node class consi 
+         
+         
+         */
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+           
+            //constructor for the node class
+        }
         static void Main(string[] args)
         {
         }
